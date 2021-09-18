460d87d1abcd5726f1b9f63e50577ea0a5de338e
Bar Tsafrir <bar7498@gmail.com>
2021-09-18 14:22:35 +0300

    On/Off build

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Container, Row, Col, Button, Input } from "reactstrap";
+import BootstrapSwitchButton from "bootstrap-switch-button-react";
+
 import a1 from "../sounds/120_future_funk_beats_25.mp3";
 import a2 from "../sounds/120_stutter_breakbeats_16.mp3";
 import a3 from "../sounds/Bass Warwick heavy funk groove on E 120 BPM.mp3";
@@ -10,11 +12,13 @@ import a7 from "../sounds/MazePolitics_120_Perc.mp3";
 import a8 from "../sounds/PAS3GROOVE1.03B.mp3";
 import a9 from "../sounds/SilentStar_120_Em_OrganSynth.mp3";
 
-function Second() {
+function Game() {
+  // Rendering a square
   const renderBox = (i) => {
-    return <Input type="checkbox" id={"box" + i} onClick={() => handleTick(i)} />;
+    return <Input type="checkbox" id={"box" + i} onClick={() => handleTick(i)} className="square" />;
   };
 
+  // Handling a square tick
   const handleTick = (i) => {
     sounds.current[i].pause();
     sounds.current[i].currentTime = 0;
@@ -23,6 +27,19 @@ function Second() {
     setMatrix(temp);
   };
 
+  // Handling the on/off switch toggle
+  const handleSwitch = (state) => {
+    if (state) {
+      console.log(state);
+      setBars(bars + 1);
+    } else {
+      console.log(state);
+      setBars(0);
+    }
+  };
+
+  // Creating an array representing all squares states (active/inactive)
+
   const initialMatrix = Array(9).fill(false);
   const matrix = useRef(initialMatrix);
 
@@ -30,21 +47,37 @@ function Second() {
     matrix.current = n;
   };
 
+  // Storing all audio objects
   const sounds = useRef([new Audio(a1), new Audio(a2), new Audio(a3), new Audio(a4), new Audio(a5), new Audio(a6), new Audio(a7), new Audio(a8), new Audio(a9)]);
+
+  // Bars counter. 0 is music off.
   const [bars, setBars] = useState(0);
 
+  // Handling bar change (to next bar / off)
   useEffect(() => {
+    let myTimeout;
     if (bars > 0) {
       console.log(matrix.current);
       matrix.current.forEach((item, index) => {
         if (item) {
-          console.log(index);
+          console.log(index + " " + Date.now());
           sounds.current[index].pause();
           sounds.current[index].currentTime = 0;
           sounds.current[index].play();
         }
       });
-      setTimeout(() => setBars(bars + 1), 8000);
+      myTimeout = setTimeout(() => {
+        setBars(bars + 1);
+      }, 8000);
+      return () => {
+        clearTimeout(myTimeout);
+      };
+    } else {
+      clearTimeout(myTimeout);
+      matrix.current.forEach((item, index) => {
+        sounds.current[index].pause();
+        sounds.current[index].currentTime = 0;
+      });
     }
   }, [bars]);
 
@@ -66,12 +99,20 @@ function Second() {
         <Col>
           <Button color="primary" onClick={() => setBars(bars + 1)}>
             Start
-          </Button>
+          </Button>{" "}
+          <BootstrapSwitchButton
+            checked={bars !== 0}
+            onstyle="success"
+            offstyle="dark"
+            onChange={(checked) => {
+              handleSwitch(checked);
+            }}
+          />
         </Col>
-        <Col>{matrix.current.join(",")}</Col>
+        <Col>{bars + "   " + matrix.current.join(",")}</Col>
       </Row>
     </Container>
   );
 }
 
-export default Second;
+export default Game;
